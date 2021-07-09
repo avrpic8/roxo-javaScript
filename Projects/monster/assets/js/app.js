41b5511ea13169233ca984b7cbd0677f574259ab
avrpic8 <avrpic8@gmail.com>
2021-07-09 23:10:46 +0430

    dealMonsterDamage and dealPlayerDamage some changes

--- Projects/monster/assets/js/app.js
+++ Projects/monster/assets/js/app.js
@@ -1,7 +1,12 @@
 const ATTACK_VALUE = 10;
 
-let chosenMaxLife = 5;
+let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjastHealthBars(chosenMaxLife);
+
+attackBtn.addEventListener("click", function () {
+    dealMonsterDamage(ATTACK_VALUE);
+});
+
--- Projects/monster/assets/js/vendor.js
+++ Projects/monster/assets/js/vendor.js
@@ -1,4 +1,3 @@
-
 // progress bars
 const monsterHealth = document.getElementById('monster-health');
 const playerHealth = document.getElementById('player-health');
@@ -6,35 +5,34 @@ const bonusLife = document.getElementById('bonus-life');
 
 
 // buttons
-const atackBtn = document.getElementById('atack-btn');
-const strongAtackBtn = document.getElementById('strong-atack-btn');
+const attackBtn = document.getElementById('attack-btn');
+const strongAttackBtn = document.getElementById('strong-atack-btn');
 const healthBtn = document.getElementById('health-btn');
 const logBtn = document.getElementById('log-btn');
 
 // functions
-
 function adjastHealthBars(maxLife) {
-    monsterHealth.max = maxLife;
-    monsterHealth.width = maxLife;
-
-    playerHealth.max = maxLife;
-    playerHealth.width = maxLife;
+    monsterHealth.setAttribute('aria-valuemax',  maxLife);
+    monsterHealth.style.width = maxLife.toString() + "%";
+    playerHealth.setAttribute('aria-valuemax', maxLife);
+    playerHealth.style.width = maxLife.toString() + "%";
 }
 
 function dealMonsterDamage(damage) {
     const dealDamage = Math.random() * damage;
-    monsterHealth.value =+ monsterHealth.value - dealDamage;
-    return dealDamage;
+    let currentHealth = parseInt(monsterHealth.style.width);
+    monsterHealth.style.width = (currentHealth - dealDamage) + "%";
 }
 
 function dealPlayerDamage(damage) {
     const dealDamage = Math.random() * damage;
-    playerHealth.value =+ playerHealth.value - dealDamage;
+    let currentHealth = parseInt(playerHealth.style.width);
+    playerHealth.style.width = (currentHealth + dealDamage) + "%";
     return dealDamage;
 }
 
 function increasePlayerHealth(healValue) {
-    playerHealth =+ playerHealth.value + healValue; 
+    playerHealth.value =+ playerHealth.value + healValue;
 }
 
 function resetGame(value) {
@@ -46,6 +44,7 @@ function removeBonusLife() {
     bonusLife.parentNode.replaceChild(bonusLife);
 }
 
-function setPlayerhealth(health) {
+function setPlayerHealth(health) {
     playerHealth.value = health;
 }
+
