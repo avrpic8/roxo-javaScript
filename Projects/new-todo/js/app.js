14674ba5133037bbd4f0dd81bd65692837d6cd09
saeed <avrpic8@gmail.com>
2021-08-04 18:13:14 +0430

    clearTasks

--- Projects/new-todo/js/app.js
+++ Projects/new-todo/js/app.js
@@ -8,11 +8,6 @@ class Ui{
         this.taskInput = document.querySelector('#task');
     }
 
-    loadEventListeners(){
-
-        this.form.addEventListener('submit', e =>this.addTask(e));
-    }
-
     addTask(e){
 
         if(this.taskInput.value === ''){
@@ -21,13 +16,13 @@ class Ui{
         }
         
         const li = document.createElement('li');
-        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.className = 'list-group-item d-flex justify-content-between align-items-center py-0';
         li.appendChild(document.createTextNode(this.taskInput.value));
         
 
         // Create new link element
         const link = document.createElement('a');
-        link.className = 'nav-link';
+        link.className = 'nav-link delete-item';
         link.innerHTML = '<i class="fa fa-remove"></i>';
         li.appendChild(link);
 
@@ -40,7 +35,34 @@ class Ui{
         e.preventDefault();
     }
 
+    removeTask(e){
+        if(e.target.parentElement.classList.contains('delete-item')){
+            if (confirm('Are You Sure?')) {
+                e.target.parentElement.parentElement.remove();
+            }
+        }
+    }
 
+    clearTasks(){ 
+
+        if(confirm('Are You Sure To Delete All Tasks?')){
+            while(this.taskList.firstChild) {
+                this.taskList.removeChild(this.taskList.firstChild);
+            } 
+        }    
+    }
+
+    loadEventListeners(){
+
+        // add task event
+        this.form.addEventListener('submit', e =>this.addTask(e));
+
+        // remove task event
+        this.taskList.addEventListener('click', e =>this.removeTask(e));
+
+        // clear tasks event
+        this.clearBtn.addEventListener('click', this.clearTasks.bind(this));
+    }
 
     run(){
 
